45f4be9296952d292953ab1125fa40680a6249f6
Ashutosh Kashyap <ashutosh.kashyap.ext@siemens.com>
2023-05-12 15:31:13 +0530

    Adding new client credentials in Identity Manager

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,5 +1,8 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -17,6 +20,30 @@ namespace IdentityServer
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = {"siemensWorksAPI"}
+                },
+                new Client
+                {
+                    ClientId = "siemensWorks_mvc_client",
+                    ClientName = "Work MVC web app",
+                    AllowedGrantTypes= GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>()
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -31,10 +58,23 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
         public static List<TestUser> TestUsers =>
             new List<TestUser>
             {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "ashu",
+                    Password = "1234",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "Ashu"),
+                        new Claim(JwtClaimTypes.FamilyName, "Kashyap")
+                    }
+                }
             };
 
     }
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -3,13 +3,21 @@ using IdentityServer4.Models;
 using IdentityServer4.Test;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(Config.Clients)
     .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddTestUsers(Config.TestUsers)
     .AddDeveloperSigningCredential();
 var app = builder.Build();
-
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
-app.MapGet("/", () => "Hello World!");
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
